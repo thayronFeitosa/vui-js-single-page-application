ee6bc99ff0ae33de2a39bcbd6904339b34a5f802
thayron matos <thayronrt@gmail.com>
2020-02-22 12:02:03 -0300

    Foi utilizado uma nova funcionalidade (v-form) para pegar todas as imagens que estiverem em um array

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
 <div>
 <h1>{{ titulo }}</h1>
-<img :src="foto.url" :alt="foto.titulo">
+
+
+<ul>
+  <li v-for="foto of fotos">
+
+    <img :src="foto.url" alt="foto.tutulo">
+  </li>
+
+
+  <img src="" alt="">
+</ul>
+
 </div>
 
 </template>
@@ -12,10 +23,19 @@ export default {
   data () {
     return {
     titulo: 'alura pic',
-    foto:{
+    fotos:[
+      
+      {
+        url: 'https://lh3.googleusercontent.com/proxy/tpJS9gOy2lsW9Yi9b_3HUTjP4l7PDp5sW7lDfzzZsAfPChj0WC73Q31diJ8qC4JqZmnIguV5a1hkT1hcLVypKs6C08fKivAeo82-_z2o75CKH10vTOsrv5nk7WKZcnEsIA',
+        titulo: 'cachorro'
+      }, 
+    {
       url: 'https://lh3.googleusercontent.com/proxy/tpJS9gOy2lsW9Yi9b_3HUTjP4l7PDp5sW7lDfzzZsAfPChj0WC73Q31diJ8qC4JqZmnIguV5a1hkT1hcLVypKs6C08fKivAeo82-_z2o75CKH10vTOsrv5nk7WKZcnEsIA',
       titulo: 'cachorro'
     }
+
+    ]
+    
     }
 
   }
