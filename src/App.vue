ea7fd8ff05c617a819c4beb53e704f5cb93deddf
thayron matos <thayronrt@gmail.com>
2020-02-22 11:17:13 -0300

    Primeiro commit sem mudar nada

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+
+<h1>alura pic</h1>
+
+
+</div>
+
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+    titulo: 'alura pic',
+    foto:{
+      url: 'https://lh3.googleusercontent.com/proxy/tpJS9gOy2lsW9Yi9b_3HUTjP4l7PDp5sW7lDfzzZsAfPChj0WC73Q31diJ8qC4JqZmnIguV5a1hkT1hcLVypKs6C08fKivAeo82-_z2o75CKH10vTOsrv5nk7WKZcnEsIA',
+      titulo: 'cachorro'
+    }
+    }
+
+  }
+}
+</script>
+
+<style>
+ 
+</style>
